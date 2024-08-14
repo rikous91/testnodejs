eccfcc5dd0ea15dd8653b0513cc7547ba5e87437
rikous91 <170421071+rikous91@users.noreply.github.com>
2024-08-14 17:50:54 +0200

    update

--- app.js
+++ app.js
@@ -1,82 +1,29 @@
-var createError = require("http-errors");
-var express = require("express");
-var mongoose = require("mongoose");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-const { format } = require("date-fns");
+// Express
+const express = require("express");
+const app = express();
+//CORS
+const cors = require('cors');
+app.use(cors()); //imposta Allow-Origin' su *,
 
-// 1st party dependencies
-var configData = require("./config/connection");
-var indexRouter = require("./routes/index");
-
-async function getApp() {
-
-  // Database
-  var connectionInfo = await configData.getConnectionInfo();
-  mongoose.connect(connectionInfo.DATABASE_URL);
-
-  var app = express();
-
-  var port = normalizePort(process.env.PORT || '3000');
+var port = process.env.PORT || '3000';
   app.set('port', port);
 
-  // view engine setup
-  app.set("views", path.join(__dirname, "views"));
-  app.set("view engine", "pug");
 
-  app.use(logger("dev"));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json({ limit: '5mb' }))
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+app.use(app.oauth.errorHandler());
 
-  app.locals.format = format;
 
-  app.use("/", indexRouter);
-  app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js")); // redirect bootstrap JS
-  app.use(
-    "/css",
-    express.static(__dirname + "/node_modules/bootstrap/dist/css")
-  ); // redirect CSS bootstrap
+//
 
-  // catch 404 and forward to error handler
-  app.use(function (req, res, next) {
-    next(createError(404));
-  });
+app.get('/init', async (req, res) => {
+return res.status(200).send('ok');
+});
 
-  // error handler
-  app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
-  });
 
-  return app;
-}
-/**
- * Normalize a port into a number, string, or false.
- */
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
 
- function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-module.exports = {
-  getApp
-};
+module.exports.app = app;
